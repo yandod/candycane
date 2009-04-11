35f696e4fc359cf19dc37bf506652f340dda4548
k-kishida <k-kishida@esm.co.jp>
2009-04-11 17:28:29 +0900

    comment to add

--- app/controllers/issues_controller.php
+++ app/controllers/issues_controller.php
@@ -146,6 +146,7 @@ class IssuesController extends AppController
 #
   function add() {
 #    @issue = Issue.new
+/*
     if(isset($this->params['copy_from'])) {
       $this->Issue->copy_from($this->params['copy_from']);
     }
@@ -171,6 +172,7 @@ class IssuesController extends AppController
       if()
       $watcher_user_ids = params['issue']['watcher_user_ids'] if User.current.allowed_to?(:add_issue_watchers, @project)
     }
+*/
 #    @issue.author = User.current
 #    
 #    default_status = IssueStatus.default
