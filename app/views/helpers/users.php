c2534cea03f6797fc046acd685b660d79070e79f
halt <halt.feits@gmail.com>
2009-04-11 16:21:15 +0900

    add base code

--- app/views/helpers/users.php
+++ app/views/helpers/users.php
@@ -15,6 +15,34 @@ class UsersHelper extends AppHelper
 {
   var $helpers = array('Html');
 
+#  def users_status_options_for_select(selected)
+#    user_count_by_status = User.count(:group => 'status').to_hash
+#    options_for_select([[l(:label_all), ''], 
+#                        ["#{l(:status_active)} (#{user_count_by_status[1].to_i})", 1],
+#                        ["#{l(:status_registered)} (#{user_count_by_status[2].to_i})", 2],
+#                        ["#{l(:status_locked)} (#{user_count_by_status[3].to_i})", 3]], selected)
+#  end
+#  
+#  # Options for the new membership projects combo-box
+#  def projects_options_for_select(projects)
+#    options = content_tag('option', "--- #{l(:actionview_instancetag_blank_option)} ---")
+#    projects_by_root = projects.group_by(&:root)
+#    projects_by_root.keys.sort.each do |root|
+#      options << content_tag('option', h(root.name), :value => root.id, :disabled => (!projects.include?(root)))
+#      projects_by_root[root].sort.each do |project|
+#        next if project == root
+#        options << content_tag('option', '&#187; ' + h(project.name), :value => project.id)
+#      end
+#    end
+#    options
+#  end
+#  
+#  def user_settings_tabs
+#    tabs = [{:name => 'general', :partial => 'users/general', :label => :label_general},
+#            {:name => 'memberships', :partial => 'users/memberships', :label => :label_project_plural}
+#            ]
+#  end
+
   /**
    * change_status_link
    *
@@ -61,5 +89,5 @@ class UsersHelper extends AppHelper
     #   :class => 'icon icon-lock'
     # end
   }
-  
+ 
 }
