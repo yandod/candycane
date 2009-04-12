07791c3035bd577989e6bd14240aac1d012ea462
k-kishida <k-kishida@esm.co.jp>
2009-04-12 10:17:49 +0900

    acts are update to proc like cake

--- app/models/behaviors/customizable.php
+++ app/models/behaviors/customizable.php
@@ -1,76 +1,10 @@
 <?php
 /*
-module Redmine
-  module Acts
-    module Customizable
-      def self.included(base)
-        base.extend ClassMethods
-      end
-
-      module ClassMethods
-        def acts_as_customizable(options = {})
-          return if self.included_modules.include?(Redmine::Acts::Customizable::InstanceMethods)
-          cattr_accessor :customizable_options
-          self.customizable_options = options
-          has_many :custom_values, :as => :customized,
-                                   :include => :custom_field,
-                                   :order => "#{CustomField.table_name}.position",
-                                   :dependent => :delete_all
-          before_validation_on_create { |customized| customized.custom_field_values }
-          # Trigger validation only if custom values were changed
-          validates_associated :custom_values, :on => :update, :if => Proc.new { |customized| customized.custom_field_values_changed? }
-          send :include, Redmine::Acts::Customizable::InstanceMethods
-          # Save custom values when saving the customized object
-          after_save :save_custom_field_values
-        end
-      end
-
-      module InstanceMethods
-        def self.included(base)
-          base.extend ClassMethods
-        end
-
-        def available_custom_fields
-          CustomField.find(:all, :conditions => "type = '#{self.class.name}CustomField'",
-                                 :order => 'position')
-        end
-
-        def custom_field_values=(values)
-          @custom_field_values_changed = true
-          values = values.stringify_keys
-          custom_field_values.each do |custom_value|
-            custom_value.value = values[custom_value.custom_field_id.to_s] if values.has_key?(custom_value.custom_field_id.to_s)
-          end if values.is_a?(Hash)
-        end
-
-        def custom_field_values
-          @custom_field_values ||= available_custom_fields.collect { |x| custom_values.detect { |v| v.custom_field == x } || custom_values.build(:custom_field => x, :value => nil) }
-        end
-
-        def custom_field_values_changed?
-          @custom_field_values_changed == true
-        end
-
-        def custom_value_for(c)
-          field_id = (c.is_a?(CustomField) ? c.id : c.to_i)
-          custom_values.detect {|v| v.custom_field_id == field_id }
-        end
-
-        def save_custom_field_values
-          custom_field_values.each(&:save)
-          @custom_field_values_changed = false
-          @custom_field_values = nil
-        end
-
-        module ClassMethods
-        end
-      end
-    end
-  end
-end
+  Customizable Behavior
 */
 class CustomizableBehavior extends ModelBehavior {
-  var $custom_field_values_changed = false;
+  // array (example)
+  // array[{n}]['CustomValue'][{fieldname}]
   var $custom_field_values = null;
   
   function setup(&$Model, $config = array()) {
@@ -78,78 +12,83 @@ class CustomizableBehavior extends ModelBehavior {
     $this->settings[$Model->alias] = $settings;
     return true;
   }
-  function beforeValidate(&$Model){
-e(pr($Model->data));
-exit;
-    # before_validation_on_create { |customized| customized.custom_field_values }
-    return true;
-  }
-  function beforeSave(&$Model) {
-    # Trigger validation only if custom values were changed
-    # validates_associated :custom_values, :on => :update, :if => Proc.new { |customized| customized.custom_field_values_changed? }
-    if(!empty($Model->id) && $this->customized.custom_field_values_changed($Model)) {
-      // TODO validate values
-    }
-    return true;
-  }
-  function afterSave(&$Model, $created) {
-    # after_save :save_custom_field_values
-    return $this->save_custom_field_values($Model, $created);
-  }
 
+  /**
+   * Check between Model->data[modelname][custom_field_values][] and Database values
+   */
+  function beforeValidate(&$Model){
+    return true;
+  }
+  /**
+   * Create save data
+   * Create data from Model->data[modelname][custom_field_values] 
+   */
+  function beforeSave(&$Model) {
+    $this->custom_field_values = $this->_create_save_data($Model);
+    return true;
+  }
+  /**
+   * Save custom field values after Main Model
+   */
+  function afterSave(&$Model, $created) {
+    return $this->_save_custom_field_values($Model, $created);
+  }
+  /**
+   * Add relation of CustomValues
+   */
+  function afterFind(&$Model, $results, $primary = false) {
+    if(!empty($results[$Model->name])) {
+      $customValueModel = & ClassRegistry::init('CustomValue');
+      $conditions = array('type'=> $Model->name, 'customized_id'=>$results[$Model->name]['id']);
+      $order = 'CustomField.position';
+      $values = $customValueModel->find('all', compact('conditions', 'order'));
+      if(!empty($values)) {
+        $results['CustomValue'] = array();
+        foreach($values as $value) {
+          $results['CustomValue'][] = $value['CustomValue'];
+        }
+      }
+    }
+    return $results;
+  }
+  
+  /**
+   * Get available field values 
+   */
   function available_custom_fields(&$Model) {
-    $Model->bindModel(array('hasMany'=>array('CustomValue'=>array('order'=>'CustomField.position', 'dependent'=>true, 'foreignKey'=>'customized_id'))), false);
-    return $Model->CustomValue->CustomField->find('all', 
+    $customValueModel = & ClassRegistry::init('CustomValue');
+    return $customValueModel->CustomField->find('all', 
         array('conditions' => array('type'=> $Model->name.'CustomField'), 'order'=>'position'));
   }
 
-  function custom_field_values(&$Model, $values) {
-    $this->custom_field_values_changed = true;
-    if(is_array($values)) {
-      $this->custom_field_values = $this->_custom_field_values(&$Model);
-      foreach($this->custom_field_values as $key => $custom_value) {
-        if(!empty($values[$custom_value['CustomValue']['custom_field_id']])) {
-          $custom_value['CustomValue']['value'] = $values[$custom_value['CustomValue']['custom_field_id']];
-        }
-      }
-    }
-  }
+   // ==== privates 
 
-  function _custom_field_values(&$Model) {
-    if(empty($this->custom_field_values)) {
-      $availables = $this->available_custom_fields($Model); 
-      foreach($availables as $x) {
-        $match = false;
-        foreach($Model->data['CustomValue'] as $v) {
-          if($v['CustomField'] == $x) {
-              // copy
-              $match = true;
-          }
-        }
-        # (custom_values.build(:custom_field => x, :value => nil) 
+  function _create_save_data(&$Model) {
+    $this->custom_field_values = array();
+    if(!empty($Model->data[$Model->name]['custom_field_values'])) {
+      // Create save data from POST data.
+      foreach($Model->data[$Model->name]['custom_field_values'] as $key => $input) {
+        $this->custom_field_values[] = array('CustomValue' => array(
+          'customized_type' => $Model->name,
+          'customized_id' => $Mpdel->id,
+          'custom_field_id' => $key,
+          'value' => $value
+        ));
       }
     }
-  }
-
-  function custom_value_for(&$Model, $c) {
-    $field_id = isset($c['CustomField']) ? $c['CustomField']['id'] : 0;
-    foreach($Model->data['CustomValue'] as $v) {
-      if($v['custom_field_id'] == $field_id) {
-        return $v;
-      }
-    }
-    return false;
   }
 
   function save_custom_field_values(&$Model, $created) {
-    $Model->bindModel(array('hasMany'=>array('CustomValue'=>array('order'=>'CustomField.position', 'dependent'=>true, 'foreignKey'=>'customized_id'))), false);
-    if(isset($Model->data['CustomValue'])) {
-      foreach($Model->data['CustomValue'] as $custom_value) {
-        $Model->CustomValue->create();
-        $Model->CustomValue->save($custom_value);
+    $customValueModel = & ClassRegistry::init('CustomValue');
+    if(!empty($this->custom_field_values)) {
+      foreach($this->custom_field_values as $custom_value) {
+        $customValueModel->create();
+        if($created) {
+          $custom_value['customized_id'] = $Model->id;
+        }
+        $customValueModel->save($custom_value);
       }
     }
-    $this->custom_field_values_changed = false;
     $this->custom_field_values = null;
   }
 }
--- app/models/behaviors/watchable.php
+++ app/models/behaviors/watchable.php
@@ -88,9 +88,13 @@ class WatchableBehavior extends ModelBehavior {
 
   # Adds user as a watcher
   function add_watcher(&$Model, $user) {
-    $Model->bindModel(array('hasMany'=>array('Watcher'=>array('dependent'=>true,'foreignKey'=>'watchable_id'))), false);
-    $Model->Watcher->create();
-    return $Model->Watcher->save(array('Watcher'=>array('watchable_type'=>$Model->name, 'watchable_id '=>$Model->id, 'user_id'=>$user['User']['id'])));
+    $model = & ClassRegistry::init('Watcher');
+    $model->create();
+    $data = array('watchable_type'=>$Model->name, 'watchable_id '=>$Model->id, 'user_id'=>$user['User']['id']);
+    if($model->hasAny($data)) {
+      return true;
+    }
+    return $Model->Watcher->save(array('Watcher'=>data));
   }
 
   # Removes user from the watchers list
@@ -98,8 +102,9 @@ class WatchableBehavior extends ModelBehavior {
     if(empty($user)) {
       return false;
     }
-    $Model->bindModel(array('hasMany'=>array('Watcher'=>array('dependent'=>true,'foreignKey'=>'watchable_id'))), false);
-    return $Model->Watcher->deleteAll(array('watchable_type'=>$Model->name, 'watchable_id'=>$Model->id, 'user_id'=>$user['User']['id']));
+    $model = & ClassRegistry::init('Watcher');
+    $conditions = array('watchable_type'=>$Model->name, 'watchable_id '=>$Model->id, 'user_id'=>$user['User']['id']);
+    return $model->deleteAll($conditions);
   }
 
   # Adds/removes watcher
@@ -109,8 +114,8 @@ class WatchableBehavior extends ModelBehavior {
 
   # Returns if object is watched by user
   function watched_by(&$Model, $user) {
-    $Model->bindModel(array('hasMany'=>array('Watcher'=>array('dependent'=>true,'foreignKey'=>'watchable_id'))), false);
-    $watcher = $Model->Watcher->find('first',array('conditions' => array("Watcher.user_id"=>$user['User']['id'])));
+    $model = & ClassRegistry::init('Watcher');
+    $watcher = $model->find('first',array('conditions' => array("Watcher.user_id"=>$user['User']['id'])));
     if(!$watcher) {
       return false;
     }
@@ -123,9 +128,9 @@ class WatchableBehavior extends ModelBehavior {
 
   # Returns an array of watchers' email addresses
   function watcher_recipients(&$Model) {
-    $Model->bindModel(array('hasMany'=>array('Watcher'=>array('dependent'=>true,'foreignKey'=>'watchable_id'))), false);
-    $Model->Watcher->bindModel(array('belongsTo'=>array('User')), false);
-    $watchers = $Model->Watcher->find('all', array('conditions' => array('watchable_type'=>$Model->name, 'watchable_id'=>$Model->id)));
+    $model = & ClassRegistry::init('Watcher');
+    $model->bindModel(array('belongsTo'=>array('User')), false);
+    $watchers = $model->find('all', array('conditions' => array('watchable_type'=>$Model->name, 'watchable_id'=>$Model->id)));
     if(empty($watchers)) {
       return array();
     }
