917045851935533fd89d74b5200ed1872b64ddd4
k-kishida <k-kishida@esm.co.jp>
2009-04-29 21:48:03 +0900

    IssueStatusModel test case passed.

--- app/tests/fixtures/issue_status_fixture.php
+++ app/tests/fixtures/issue_status_fixture.php
@@ -10,12 +10,12 @@ class IssueStatusFixture extends CakeTestFixture {
       'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1))
   );
   var $records = array(
-    array('name'=>"Rejected", 'is_default'=>false, 'is_closed'=>true, 'id'=>6),
-    array('name'=>"New",      'is_default'=>true,  'is_closed'=>false,'id'=>1),
-    array('name'=>"Assigned", 'is_default'=>false, 'is_closed'=>false,'id'=>2),
-    array('name'=>"Resolved", 'is_default'=>false, 'is_closed'=>false,'id'=>3),
-    array('name'=>"Feedback", 'is_default'=>false, 'is_closed'=>false,'id'=>4),
-    array('name'=>"Closed",   'is_default'=>false, 'is_closed'=>true, 'id'=>5),
+    array('name'=>"Rejected", 'is_default'=>false, 'is_closed'=>true, 'id'=>6, 'position'=>1),
+    array('name'=>"New",      'is_default'=>true,  'is_closed'=>false,'id'=>1, 'position'=>2),
+    array('name'=>"Assigned", 'is_default'=>false, 'is_closed'=>false,'id'=>2, 'position'=>3),
+    array('name'=>"Resolved", 'is_default'=>false, 'is_closed'=>false,'id'=>3, 'position'=>4),
+    array('name'=>"Feedback", 'is_default'=>false, 'is_closed'=>false,'id'=>4, 'position'=>5),
+    array('name'=>"Closed",   'is_default'=>false, 'is_closed'=>true, 'id'=>5, 'position'=>6),
   );
 }
 ?>
