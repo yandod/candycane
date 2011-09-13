f94e425f2ca5308f9cd7daec40bd1a81be2964c3
Graham Weldon <graham@grahamweldon.com>
2011-09-13 12:40:24 +1000

    Super minor change to AuthSource.

--- app/models/auth_source.php
+++ app/models/auth_source.php
@@ -16,8 +16,7 @@
 ## along with this program; if not, write to the Free Software
 ## Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
-class AuthSource extends AppModel
-{
+class AuthSource extends AppModel {
 #  has_many :users
 #  
 #  validates_presence_of :name
