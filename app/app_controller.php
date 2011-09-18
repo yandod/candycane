b9ddc2c67dba8e3e41eb8b413cf7736dd9b0ed2e
Graham Weldon <graham@grahamweldon.com>
2011-09-18 18:09:54 +1000

    Code cleanup for AppController

--- app/app_controller.php
+++ app/app_controller.php
@@ -62,175 +62,203 @@ class AppController extends Controller {
 #    @current_role ||= User.current.role_for_project(@project)
 #  end
 #
-    /**
-     * user_setup
-     *
-     * @todo Setting.check_cache
-     */
-    function user_setup()
-    {
-#    # Check the settings cache for each request
-#    Setting.check_cache
-        
-        // Find the current user
-        $this->current_user = $this->find_current_user();
-        $this->set('currentuser', $this->current_user);
-    }
 
-    /**
-     * find_current_user
-     *
-     * Returns the current user or nil if no user is logged in
-     *
-     * @todo Setting.autologin
-     * @todo auto_login
-     * @todo rss key authentication
-     */
-    function _find_current_user() {
+/**
+ * User Setup
+ *
+ * @return void
+ * @todo Setting.check_cache
+ */
+	public function user_setup() {
+		#    # Check the settings cache for each request
+		#    Setting.check_cache
 
-      if ($this->Session->read('user_id')) {
-        // existing session
-        return $this->User->find_by_id_logged($this->Session->read('user_id'));
-#      (User.active.find(session[:user_id]) rescue nil)
-      } else if ($this->Cookie->read('autologin')) {
-#    elsif cookies[:autologin] && Setting.autologin?
-#      # auto-login feature
-#      User.find_by_autologin_key(cookies[:autologin])
-      } elseif (!empty($this->params['url']['key'])) {
-        // from rss reader
-        $user = $this->User->find_by_rss_key($this->params['url']['key']);
-        if(!empty($user)) {
-          $user = $this->User->find_by_id_logged($user['id']);
-        }
-        if(empty($user)) {
-          $this->cakeError('error404');
-        }
-        return $user;
-      } else {
-        $user = $this->User->anonymous();
-        $user['User']['logged'] = false;
-        $user['User']['name'] = $user['User']['login'];
-        $user['User']['memberships'] = array();
-        return $user['User'];
-      }
-      return null;
-    }
-    function find_current_user() {
-      return $this->_find_current_user();
-    }
+		// Find the current user
+		$this->current_user = $this->find_current_user();
+		$this->set('currentuser', $this->current_user);
+	}
 
-    /**
-     * check_if_login_required
-     *
-     * check if login is globally required to access the application
-     *
-     * @todo implement Setting.login_required
-     * @todo logged?
-     */
-    function check_if_login_required()
-    {
-        // no check needed if user is already logged in
-        if ($this->current_user['logged']) {
-            return true;
-        }
+/**
+ * Find the current logged in user
+ *
+ * Returns the current user or nil if no user is logged in
+ *
+ * @return void
+ * @todo Setting.autologin
+ * @todo auto_login
+ * @todo rss key authentication
+ */
+	function _find_current_user() {
+		if ($this->Session->read('user_id')) {
+			// existing session
+			return $this->User->find_by_id_logged($this->Session->read('user_id'));
+			# (User.active.find(session[:user_id]) rescue nil)
+		} else if ($this->Cookie->read('autologin')) {
+			# elsif cookies[:autologin] && Setting.autologin?
+			#      # auto-login feature
+			#      User.find_by_autologin_key(cookies[:autologin])
+		} elseif (!empty($this->params['url']['key'])) {
+			// from rss reader
+			$user = $this->User->find_by_rss_key($this->params['url']['key']);
+			if (!empty($user)) {
+				$user = $this->User->find_by_id_logged($user['id']);
+			}
+			if (empty($user)) {
+				$this->cakeError('error404');
+			}
+			return $user;
+		} else {
+			$user = $this->User->anonymous();
+			$user['User']['logged'] = false;
+			$user['User']['name'] = $user['User']['login'];
+			$user['User']['memberships'] = array();
+			return $user['User'];
+		}
+		return null;
+	}
 
-        if ($this->Setting->login_required) {
-            $this->require_login();
-        }
-    }
+/**
+ * Find current user
+ *
+ * @return mixed Current User
+ */
+	public function find_current_user() {
+		return $this->_find_current_user();
+	}
 
-  function set_localization()
-  {
-#    User.current.language = nil unless User.current.logged?
-#    lang = begin
-#      if !User.current.language.blank? && GLoc.valid_language?(User.current.language)
-#        User.current.language
-#      elsif request.env['HTTP_ACCEPT_LANGUAGE']
-#        accept_lang = parse_qvalues(request.env['HTTP_ACCEPT_LANGUAGE']).first.downcase
-#        if !accept_lang.blank? && (GLoc.valid_language?(accept_lang) || GLoc.valid_language?(accept_lang = accept_lang.split('-').first))
-#          User.current.language = accept_lang
-#        end
-#      end
-#    rescue
-#      nil
-#    end || Setting.default_language
-#    set_language_if_valid(lang)
-    $lang = null;
-    if ( !empty($this->current_user['language']) ) {
-      $lang = $this->current_user['language'];
-    } elseif (!empty($this->Setting->default_language)) {
-  	  $lang = $this->Setting->default_language;
-  	}
-  	$this->L10n = new L10n();
-  	$this->L10n->get($lang);
-  	Configure::write('Config.language',$lang);
-  }
-  
-    /**
-     * require_login
-     *
-     * @todo set back_url
-     */
-    function require_login()
-    {
-        if (!$this->current_user || !$this->current_user['logged']) {
-            $this->redirect('/account/login');
-#      redirect_to :controller => "account", :action => "login", :back_url => url_for(params)
-            return false;
-        }
+/**
+ * check_if_login_required
+ *
+ * check if login is globally required to access the application
+ *
+ * @todo implement Setting.login_required
+ * @todo logged?
+ */
+/**
+ * Check if login is required
+ *
+ * check if login is globally required to access the application
+ *
+ * @return mixed
+ */
+	public function check_if_login_required() {
+		// no check needed if user is already logged in
+		if ($this->current_user['logged']) {
+			return true;
+		}
 
-        return true;
-    }
-    
-    /**
-     * require_admin
-     *
-     */
-    function require_admin()
-    {
-        if (!$this->require_login()) {
-            return false;
-        }
+		if ($this->Setting->login_required) {
+			$this->require_login();
+		}
+	}
 
-        if ($this->current_user['admin'] != 1) {
-          $this->redirect('/', 403);
-          return false;
-        }
+/**
+ * Set Localization
+ *
+ * @return void
+ */
+	public function set_localization() {
+		#    User.current.language = nil unless User.current.logged?
+		#    lang = begin
+		#      if !User.current.language.blank? && GLoc.valid_language?(User.current.language)
+		#        User.current.language
+		#      elsif request.env['HTTP_ACCEPT_LANGUAGE']
+		#        accept_lang = parse_qvalues(request.env['HTTP_ACCEPT_LANGUAGE']).first.downcase
+		#        if !accept_lang.blank? && (GLoc.valid_language?(accept_lang) || GLoc.valid_language?(accept_lang = accept_lang.split('-').first))
+		#          User.current.language = accept_lang
+		#        end
+		#      end
+		#    rescue
+		#      nil
+		#    end || Setting.default_language
+		#    set_language_if_valid(lang)
+		$lang = null;
+		if (!empty($this->current_user['language'])) {
+			$lang = $this->current_user['language'];
+		} elseif (!empty($this->Setting->default_language)) {
+			$lang = $this->Setting->default_language;
+		}
+		$this->L10n = new L10n();
+		$this->L10n->get($lang);
+		Configure::write('Config.language',$lang);
+	}
 
-        return true;
-    }
-  
-  function deny_access() {
-    return $this->current_user['logged'] ? $this->cakeError('error_403') : $this->require_login();
-  }
+/**
+ * Require Login
+ *
+ * @return boolean
+ * @todo set back_url
+ */
+	public function require_login() {
+		if (!$this->current_user || !$this->current_user['logged']) {
+			$this->redirect('/account/login');
+			#      redirect_to :controller => "account", :action => "login", :back_url => url_for(params)
+			return false;
+		}
 
-  # Authorize the user for the requested action
-  function _authorize($ctrl = false, $action = false) {
-    if(!empty($this->params['requested'])) {
-      return true;
-    }
-    if ($this->authorize === false) {
-      return true;
-    }
-    if ($ctrl === false) {
-      $ctrl = $this->params['controller'];
-    }
-    if ($action === false) {
-      $action = $this->params['action'];
-    }
-    if ($action == 'add') {
-      $action = 'new';
-    }
-    $authorize = array_merge(array('only'=>array(), 'except'=>array()), $this->authorize);
-    extract($authorize);
-    
-    if ((!empty($only) && !in_array($action, $only)) || (!empty($except) && in_array($action, $except)) ) {
-      return true;
-    }
-    $allowed = $this->User->is_allowed_to($this->current_user, array('controller' => $ctrl, 'action' => $action), $this->_project);
-    return $allowed ? true : $this->deny_access();
-  }
-  
+		return true;
+	}
+
+/**
+ * Require Admin
+ *
+ * @return boolean
+ */
+	public function require_admin() {
+		if (!$this->require_login()) {
+			return false;
+		}
+
+		if ($this->current_user['admin'] != 1) {
+			$this->redirect('/', 403);
+			return false;
+		}
+
+		return true;
+	}
+
+/**
+ * Deny Access
+ *
+ * @return mixed
+ */
+	public function deny_access() {
+		return $this->current_user['logged'] ? $this->cakeError('error_403') : $this->require_login();
+	}
+
+/**
+ * Authorize the user for the requested action
+ *
+ * @param string $ctrl Controller
+ * @param string $action Action
+ * @return boolean Allowed access
+ */
+	function _authorize($ctrl = false, $action = false) {
+		if (!empty($this->params['requested'])) {
+			return true;
+		}
+		if ($this->authorize === false) {
+			return true;
+		}
+		if ($ctrl === false) {
+			$ctrl = $this->params['controller'];
+		}
+		if ($action === false) {
+			$action = $this->params['action'];
+		}
+		if ($action == 'add') {
+			$action = 'new';
+		}
+		$authorize = array_merge(array('only' => array(), 'except' => array()), $this->authorize);
+		extract($authorize);
+
+		if ((!empty($only) && !in_array($action, $only)) || (!empty($except) && in_array($action, $except)) ) {
+			return true;
+		}
+		$allowed = $this->User->is_allowed_to($this->current_user, array('controller' => $ctrl, 'action' => $action), $this->_project);
+		return $allowed ? true : $this->deny_access();
+	}
+
 #  # make sure that the user is a member of the project (or admin) if project is private
 #  # used as a before_filter for actions that do not require any particular permission on the project
 #  def check_project_privacy
@@ -247,32 +275,48 @@ class AppController extends Controller {
 #    end
 #  end
 #
-  function redirect_back_or_default($default_url) {
-    if(!empty($this->data['back_url'])) {
-      $back_url = urldecode($this->data['back_url']);
-      $uri = parse_url($back_url);
-      # do not redirect user to another host or to the login or register page
-      # TODO relative
-      if (($uri['host'] == env('HTTP_HOST')) && !preg_match('/(login|account\/register)$/', $uri['path'])) {
-        $this->redirect($back_url);
-      }
-    }
-    $this->redirect($default_url);
-  }
-  function render_feed($event_model, $items, $options=array()) {
-    if (!($options['sort'] === false)) {
-      usort($items, array($event_model, 'cmp_event_datetime'));
-    }
-    unset($options['sort']);
-    $items = array_reverse($items);
-    $items = array_slice($items, 0, $this->Setting->feeds_limit);
-    $atom_title = !empty($options['title']) ? $options['title'] : $this->Setting->app_title;
-    $this->set(compact('atom_title', 'items'));
-    $this->set('EventModel', $event_model);
-    $this->helpers = array('Candy', 'Xml', 'Time');
-    $this->layout = 'rss/atom';
-    $this->render("/common/feed.atom");
-  }
+
+/**
+ * redirect_back_or_default
+ *
+ * @param string $default_url 
+ * @return void
+ */
+	public function redirect_back_or_default($default_url) {
+		if (!empty($this->data['back_url'])) {
+			$back_url = urldecode($this->data['back_url']);
+			$uri = parse_url($back_url);
+			# do not redirect user to another host or to the login or register page
+			# TODO relative
+			if (($uri['host'] == env('HTTP_HOST')) && !preg_match('/(login|account\/register)$/', $uri['path'])) {
+				$this->redirect($back_url);
+			}
+		}
+		$this->redirect($default_url);
+	}
+
+/**
+ * Render Feed
+ *
+ * @param string $event_model 
+ * @param array $items Items
+ * @param array $options Options
+ * @return void
+ */
+	public function render_feed($event_model, $items, $options=array()) {
+		if (!($options['sort'] === false)) {
+			usort($items, array($event_model, 'cmp_event_datetime'));
+		}
+		unset($options['sort']);
+		$items = array_reverse($items);
+		$items = array_slice($items, 0, $this->Setting->feeds_limit);
+		$atom_title = !empty($options['title']) ? $options['title'] : $this->Setting->app_title;
+		$this->set(compact('atom_title', 'items'));
+		$this->set('EventModel', $event_model);
+		$this->helpers = array('Candy', 'Xml', 'Time');
+		$this->layout = 'rss/atom';
+		$this->render("/common/feed.atom");
+	}
   
 #  def render_403
 #    @project = nil
@@ -328,47 +372,52 @@ class AppController extends Controller {
 #    attached
 #  end
 
-  /**
-   * The presence of the parameter is checked in the following order, and the value matched first is returned.
-   * 
-   * - $this->params[$name]
-   * - $this->params['named'][$name]
-   * - $this->params['url'][$name]
-   * - $this->data[$this->{$this->modelClass}->name][$name]
-   *
-   * @return null if $name is not found. 
-   */
-  function _get_param($name) {
-    if(array_key_exists($name, $this->params)) {
-      $value = $this->params[$name];
-    } elseif(array_key_exists('named', $this->params) && array_key_exists($name, $this->params['named'])) {
-      $value = $this->params['named'][$name];
-    } elseif(array_key_exists('url', $this->params) && array_key_exists($name, $this->params['url'])) {
-      $value = $this->params['url'][$name];
-    } elseif(is_array($this->data) && array_key_exists($this->{$this->modelClass}->name, $this->data) && array_key_exists($name, $this->data[$this->{$this->modelClass}->name])) {
-      $value = $this->data[$this->{$this->modelClass}->name][$name];
-    } elseif(array_key_exists('form', $this->params) && array_key_exists($name, $this->params['form'])) {
-      $value = $this->params['form'][$name];
-    } else {
-      $value = null;
-    }
-    return $value;
-  }
+/**
+ * The presence of the parameter is checked in the following order, and the value matched first is returned.
+ * 
+ * - $this->params[$name]
+ * - $this->params['named'][$name]
+ * - $this->params['url'][$name]
+ * - $this->data[$this->{$this->modelClass}->name][$name]
+ *
+ * @param string $name 
+ * @return null if $name is not found. 
+ */
+	function _get_param($name) {
+		if (array_key_exists($name, $this->params)) {
+			$value = $this->params[$name];
+		} elseif (array_key_exists('named', $this->params) && array_key_exists($name, $this->params['named'])) {
+			$value = $this->params['named'][$name];
+		} elseif (array_key_exists('url', $this->params) && array_key_exists($name, $this->params['url'])) {
+			$value = $this->params['url'][$name];
+		} elseif (is_array($this->data) && array_key_exists($this->{$this->modelClass}->name, $this->data) && array_key_exists($name, $this->data[$this->{$this->modelClass}->name])) {
+			$value = $this->data[$this->{$this->modelClass}->name][$name];
+		} elseif (array_key_exists('form', $this->params) && array_key_exists($name, $this->params['form'])) {
+			$value = $this->params['form'][$name];
+		} else {
+			$value = null;
+		}
+		return $value;
+	}
 
-  # Returns the number of objects that should be displayed
-  # on the paginated list
-  function _per_page_option()
-  {
-    if (isset($this->params['url']['per_page']) && in_array($this->params['url']['per_page'], $this->Setting->per_page_options)) {
-      $this->per_page = (int)$this->params['url']['per_page'];
-      $this->Session->write('per_page', $this->per_page);
-    } else if (strlen($this->Session->read('per_page'))) {
-      $this->per_page = $this->Session->read('per_page');
-    } else {
-      $this->per_page = $this->Setting->per_page_options[0];
-    }
-    return $this->per_page;
-  }
+/**
+ * Returns the number of objects that should be displayed
+ * on the paginated list
+ *
+ * @return int Number of objects to be displayed
+ */
+	function _per_page_option() {
+		if (isset($this->params['url']['per_page']) && in_array($this->params['url']['per_page'], $this->Setting->per_page_options)) {
+			$this->per_page = (int)$this->params['url']['per_page'];
+			$this->Session->write('per_page', $this->per_page);
+		} else if (strlen($this->Session->read('per_page'))) {
+			$this->per_page = $this->Session->read('per_page');
+		} else {
+			$this->per_page = $this->Setting->per_page_options[0];
+		}
+		return $this->per_page;
+	}
+
 #  def per_page_option
 #    per_page = nil
 #    if params[:per_page] && Setting.per_page_options_array.include?(params[:per_page].to_s.to_i)
@@ -405,37 +454,50 @@ class AppController extends Controller {
 #  def filename_for_content_disposition(name)
 #    request.env['HTTP_USER_AGENT'] =~ %r{MSIE} ? ERB::Util.url_encode(name) : name
 #  end
-  function setSettings()
-  {
-  	$this->theme = strtolower($this->Setting->ui_theme);
-  	$this->set('Settings',$this->Setting);
-  }
-  
-  function _findProject()
-  {
-    $project_id = $this->_get_param('project_id');
-    if ( !empty($project_id) ) {
-      if ($this->_project = $this->Project->findMainProject($project_id)) {
-	      $this->set(array('main_project'=> $this->_project));
-	      $this->set('main_project', $this->_project);
-	    } else {
-	      $this->cakeError('error404');
-	    }
-	  if (!$this->_isVisible($this->_project['Project']['id'])) {
-	      $this->cakeError('error404');	      
-	  }
-    }
 
-  }
-  function _isVisible($project_id){
-      $cond = $this->Project->get_visible_by_condition($this->current_user);
-      $cond['Project.id'] = $project_id;
-      $visible = $this->Project->find('first',array('conditions' => $cond));
-      if ($visible == false) {
-          return false;        
-      };
-      return true;
-  }
-  
+/**
+ * Set Settings
+ *
+ * @return void
+ */
+	public function setSettings() {
+		$this->theme = strtolower($this->Setting->ui_theme);
+		$this->set('Settings',$this->Setting);
+	}
 
+/**
+ * Find Projects
+ *
+ * @return void
+ */
+	function _findProject() {
+		$project_id = $this->_get_param('project_id');
+		if (!empty($project_id)) {
+			if ($this->_project = $this->Project->findMainProject($project_id)) {
+				$this->set(array('main_project'=> $this->_project));
+				$this->set('main_project', $this->_project);
+			} else {
+				$this->cakeError('error404');
+			}
+			if (!$this->_isVisible($this->_project['Project']['id'])) {
+				$this->cakeError('error404');	      
+			}
+		}
+	}
+
+/**
+ * Is Visible
+ *
+ * @param string $project_id Project ID
+ * @return boolean Visible
+ */
+	function _isVisible($project_id) {
+		$cond = $this->Project->get_visible_by_condition($this->current_user);
+		$cond['Project.id'] = $project_id;
+		$visible = $this->Project->find('first', array('conditions' => $cond));
+		if ($visible == false) {
+			return false;        
+		}
+		return true;
+	}
 }
