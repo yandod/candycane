63e9a7b96978b768eef565f064d25b71a77066f8
Graham Weldon <graham@grahamweldon.com>
2011-09-20 00:27:35 +1000

    Docblocks and code cleanup for mime type class.

--- app/mime_type.php
+++ app/mime_type.php
@@ -16,69 +16,105 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-class MimeType
-{
-  var $MIME_TYPES = array(
-    'text/plain' => 'txt,tpl,properties,patch,diff,ini,readme,install,upgrade',
-    'text/css' => 'css',
-    'text/html' => 'html,htm,xhtml',
-    'text/jsp' => 'jsp',
-    'text/x-c' => 'c,cpp,cc,h,hh',
-    'text/x-java' => 'java',
-    'text/x-javascript' => 'js',
-    'text/x-html-template' => 'rhtml',
-    'text/x-perl' => 'pl,pm',
-    'text/x-php' => 'php,php3,php4,php5',
-    'text/x-python' => 'py',
-    'text/x-ruby' => 'rb,rbw,ruby,rake',
-    'text/x-csh' => 'csh',
-    'text/x-sh' => 'sh',
-    'text/xml' => 'xml,xsd,mxml',
-    'text/yaml' => 'yml,yaml',
-    'image/gif' => 'gif',
-    'image/jpeg' => 'jpg,jpeg,jpe',
-    'image/png' => 'png',
-    'image/tiff' => 'tiff,tif',
-    'image/x-ms-bmp' => 'bmp',
-    'image/x-xpixmap' => 'xpm',
-  );
-  
-  var $EXTENSIONS = array();
-  
-  function __construct() {
-    foreach ($this->MIME_TYPES as $type=>$exts) {
-      foreach (split(',', $exts) as $ext) {
-        $this->EXTENSIONS[trim($ext)] = $type;
-      }
-    }
-  }
-  
-  # returns mime type for name or nil if unknown
-  function of($name) {
-    if (empty($name)) {
-      return null;
-    }
-    if (preg_match('/(^|\.)([^\.]+)$/', $name, $m)) {
-      if (!empty($this->EXTENSIONS[strtolower($m[2])])) {
-        return $this->EXTENSIONS[strtolower($m[2])];
-      }
-    }
-    return null;
-  }
-  
-  function main_mimetype_of($name) {
-    $mimetype = $this->of($name);
-    if ($mimetype) {
-      return array_shift(split('/', $mimetype));
-    }
-    return null;
-  }
-  
-  # return true if mime-type for name is type/*
-  # otherwise false
-  static function is_type($type, $name) {
-    $_this = new MimeType();
-    $main_mimetype = $_this->main_mimetype_of($name);
-    return $type == $main_mimetype;
-  }
+/**
+ * Mime Type Object
+ *
+ * @package candycane
+ */
+class MimeType {
+
+/**
+ * Mime types
+ *
+ * @var array
+ */
+	public $MIME_TYPES = array(
+		'text/plain' => 'txt,tpl,properties,patch,diff,ini,readme,install,upgrade',
+		'text/css' => 'css',
+		'text/html' => 'html,htm,xhtml',
+		'text/jsp' => 'jsp',
+		'text/x-c' => 'c,cpp,cc,h,hh',
+		'text/x-java' => 'java',
+		'text/x-javascript' => 'js',
+		'text/x-html-template' => 'rhtml',
+		'text/x-perl' => 'pl,pm',
+		'text/x-php' => 'php,php3,php4,php5',
+		'text/x-python' => 'py',
+		'text/x-ruby' => 'rb,rbw,ruby,rake',
+		'text/x-csh' => 'csh',
+		'text/x-sh' => 'sh',
+		'text/xml' => 'xml,xsd,mxml',
+		'text/yaml' => 'yml,yaml',
+		'image/gif' => 'gif',
+		'image/jpeg' => 'jpg,jpeg,jpe',
+		'image/png' => 'png',
+		'image/tiff' => 'tiff,tif',
+		'image/x-ms-bmp' => 'bmp',
+		'image/x-xpixmap' => 'xpm',
+	);
+
+/**
+ * Extensions
+ *
+ * @var array
+ */
+ 	public $EXTENSIONS = array();
+
+/**
+ * Constructor
+ *
+ */
+	public function __construct() {
+		foreach ($this->MIME_TYPES as $type => $exts) {
+			foreach (split(',', $exts) as $ext) {
+				$this->EXTENSIONS[trim($ext)] = $type;
+			}
+		}
+	}
+
+/**
+ * Returns mime type for name or nil if unknown
+ *
+ * @param string $name 
+ * @return mixed
+ */
+	public function of($name) {
+		if (empty($name)) {
+			return null;
+		}
+		if (preg_match('/(^|\.)([^\.]+)$/', $name, $m)) {
+			if (!empty($this->EXTENSIONS[strtolower($m[2])])) {
+				return $this->EXTENSIONS[strtolower($m[2])];
+			}
+		}
+		return null;
+	}
+
+/**
+ * Main Mimetype of
+ *
+ * @param string $name 
+ * @return mixed
+ */
+	public function main_mimetype_of($name) {
+		$mimetype = $this->of($name);
+		if ($mimetype) {
+			return array_shift(split('/', $mimetype));
+		}
+		return null;
+	}
+
+
+/**
+ * Return true if mime-type for name is type/* otherwise false
+ *
+ * @param string $type 
+ * @param string $name 
+ * @return boolean Mime type is type/*
+ */
+	public static function is_type($type, $name) {
+		$_this = new MimeType();
+		$main_mimetype = $_this->main_mimetype_of($name);
+		return $type == $main_mimetype;
+	}
 }
