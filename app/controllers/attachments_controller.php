85cc51c35d808318303db3dc8b15e691d7ea1315
Graham Weldon <graham@grahamweldon.com>
2011-10-03 10:51:39 +1100

    Code cleanup of Attachments Controller

--- app/controllers/attachments_controller.php
+++ app/controllers/attachments_controller.php
@@ -16,96 +16,148 @@
 ## along with this program; if not, write to the Free Software
 ## Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-class AttachmentsController extends AppController
-{
-  var $name = 'Attachments';
-  var $helpers = array(
-    'UnifiedDiff',
-    'Number',
-  );
-  function beforeFilter()
-  {
-    $this->_find_project();
-    parent::beforeFilter();
+/**
+ * Attachments Controller
+ *
+ * @package candycane
+ * @subpackage candycane.controllers
+ */
+class AttachmentsController extends AppController {
 
-    switch ($this->action) {
-    case 'destroy':
-      $this->_delete_authorize();
-      break;
-    default :
-      $this->_read_authorize();
-      break;
-    }
-    return true;
-  }
+/**
+ * Controller name
+ *
+ * @var string
+ */
+ 	public $name = 'Attachments';
 
-  function show() {
-    if ($this->Attachment->is_diff()) {
-      $diff = @file($this->Attachment->diskfile());
-      $this->set('diff', $diff);
-      $this->set('diff_type', $this->_get_param('diff_type'));
-      
-      $this->render('diff');
-    } elseif ($this->Attachment->is_text()) {
-      $content = @file($this->Attachment->diskfile());
-      $this->set('content', $content);
-      $this->render('file');
-    } else {
-      $this->download();
-    }
-  }
-  
-  function download() {
-    $data = $this->Attachment->data[$this->Attachment->alias];
-    if ($data['container_type'] == 'Version' || $data['container_type'] == 'Project') {
-      $this->Attachment->increment_download();
-    }
-    $path = pathinfo($this->Attachment->diskfile());
-    $this->view = 'Media';
-    $params = array(
-      'id' => $data['disk_filename'],
-      'name' => basename($data['filename'], '.'.$path['extension']),
-      'download' => !$this->Attachment->is_image(),
-      'path' => $path['dirname'].DS,
-      'extension' => $path['extension'],
-    );
-    $this->set($params);
-  }
-  
-  function destroy() {
-    # Make sure association callbacks are called
-    $this->Attachment->del();
-    if ($this->referer(false)) {
-      $this->redirect($this->referer());
-    } else {
-      $this->redirect(array('controller' => 'projects', 'action' => 'show', 'id' => $this->params['project_id']));
-    }
-  }
-  
-#private
-  function _find_project() {
-    $this->Attachment->read(null, $this->params['id']);
-    $this->set('attachment', $this->Attachment->data[$this->Attachment->alias]);
-    $this->set('author', $this->Attachment->data['Author']);
-    # Show 404 if the filename in the url is wrong
-    if (!empty($this->params['filename']) && $this->params['filename'] != $this->Attachment->data[$this->Attachment->name]['filename']) {
-      $this->cakeError('error404');
-    }
-    $project = $this->Attachment->project();
-    if (!empty($project['Project']['identifier'])) {
-      $this->params['project_id'] = $project['Project']['identifier'];
-      parent::_findProject();
-    } else {
-      $this->cakeError('error404');
-    }
-  }
-  
-  function _read_authorize() {
-    $this->Attachment->is_visible($this->current_user, $this->_project) ?  true : $this->deny_access();
-  }
-  
-  function _delete_authorize() {
-    $this->Attachment->is_deletable($this->current_user, $this->_project) ?  true : $this->deny_access();
-  }
+/**
+ * Helpers
+ *
+ * @var array
+ */
+ 	public $helpers = array(
+		'UnifiedDiff',
+		'Number',
+	);
 
+/**
+ * BeforeFilter
+ *
+ * @return void
+ */
+	public function beforeFilter() {
+		$this->_find_project();
+		parent::beforeFilter();
+
+		switch ($this->action) {
+			case 'destroy':
+				$this->_delete_authorize();
+				break;
+			default:
+				$this->_read_authorize();
+				break;
+		}
+		return true;
+	}
+
+/**
+ * Show action
+ *
+ * @return void
+ */
+	function show() {
+		if ($this->Attachment->is_diff()) {
+			$diff = @file($this->Attachment->diskfile());
+			$this->set('diff', $diff);
+			$this->set('diff_type', $this->_get_param('diff_type'));
+
+			$this->render('diff');
+		} elseif ($this->Attachment->is_text()) {
+			$content = @file($this->Attachment->diskfile());
+			$this->set('content', $content);
+			$this->render('file');
+		} else {
+			$this->download();
+		}
+	}
+
+/**
+ * Download action
+ *
+ * @return void
+ */
+	function download() {
+		$data = $this->Attachment->data[$this->Attachment->alias];
+		if ($data['container_type'] == 'Version' || $data['container_type'] == 'Project') {
+			$this->Attachment->increment_download();
+		}
+		$path = pathinfo($this->Attachment->diskfile());
+		$this->view = 'Media';
+		$params = array(
+			'id' => $data['disk_filename'],
+			'name' => basename($data['filename'], '.'.$path['extension']),
+			'download' => !$this->Attachment->is_image(),
+			'path' => $path['dirname'] . DS,
+			'extension' => $path['extension'],
+		);
+		$this->set($params);
+	}
+
+/**
+ * Destroy action
+ *
+ * @return void
+ */
+	function destroy() {
+		// Make sure association callbacks are called
+		$this->Attachment->del();
+		if ($this->referer(false)) {
+			$this->redirect($this->referer());
+		} else {
+			$this->redirect(array('controller' => 'projects', 'action' => 'show', 'id' => $this->params['project_id']));
+		}
+	}
+
+/**
+ * Find a project
+ *
+ * @return void
+ * @access private
+ */
+	function _find_project() {
+		$this->Attachment->read(null, $this->params['id']);
+		$this->set('attachment', $this->Attachment->data[$this->Attachment->alias]);
+		$this->set('author', $this->Attachment->data['Author']);
+
+		// Show 404 if the filename in the url is wrong
+		if (!empty($this->params['filename']) && $this->params['filename'] != $this->Attachment->data[$this->Attachment->name]['filename']) {
+			$this->cakeError('error404');
+		}
+		$project = $this->Attachment->project();
+		if (!empty($project['Project']['identifier'])) {
+			$this->params['project_id'] = $project['Project']['identifier'];
+			parent::_findProject();
+		} else {
+			$this->cakeError('error404');
+		}
+	}
+
+/**
+ * Check for read authorization
+ *
+ * @return boolean True if user can read
+ */
+	function _read_authorize() {
+		$this->Attachment->is_visible($this->current_user, $this->_project) ?  true : $this->deny_access();
+	}
+
+/**
+ * Check for delete authorization
+ *
+ * @return boolean True if user can delete
+ */
+	function _delete_authorize() {
+		$this->Attachment->is_deletable($this->current_user, $this->_project) ?  true : $this->deny_access();
+	}
 }
