97e55c9dc6aa40e3778902aabfa651cceea7d363
Yusuke Ando <yandod@gmail.com>
2011-11-13 02:32:10 +0900

    fixing code style.

--- app/models/behaviors/candy.php
+++ app/models/behaviors/candy.php
@@ -1,11 +1,14 @@
 <?php
 class CandyBehavior extends ModelBehavior
 {
-  function defaults($model)
-  {
-    $results = aa($model->name, a());
-    foreach ($model->_schema as $k => $v) $results[$model->name][$k] = $v['default'];
-    $results = $model->afterFind(array($results), true);
-    return $results[0][$model->name];
-  }
+
+	public function defaults($model) {
+		$results = aa($model->name, a());
+		foreach ($model->_schema as $k => $v) {
+			$results[$model->name][$k] = $v['default'];
+		}
+		$results = $model->afterFind(array($results), true);
+		return $results[0][$model->name];
+	}
+
 }
