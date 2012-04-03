f0138cab9c8c74634b0987d35dc7d367cf169c18
Yusuke Ando <yandod@gmail.com>
2012-04-03 12:01:01 +0900

    moved tests

--- app/tests/fixtures/wiki_content_version_fixture.php
+++
@@ -1,7 +0,0 @@
-<?php 
-class WikiContentVersionFixture extends CakeTestFixture {
-  var $name = 'WikiContentVersion';
-  var $import = array('table'=>'wiki_content_versions');
-  var $records = array(
-  );
-}
--- app/tests/fixtures/wiki_redirect_fixture.php
+++
@@ -1,7 +0,0 @@
-<?php 
-class WikiRedirectFixture extends CakeTestFixture {
-  var $name = 'WikiRedirect';
-  var $import = array('table'=>'wiki_redirects');
-  var $records = array(
-  );
-}
