0476efdb26cde9df7d893dcc69cfeed70f182cdd
Yusuke Ando <yandod@gmail.com>
2012-05-05 15:11:25 +0900

    fix fatal error in JounalsHelper

--- app/View/Helper/JournalsHelper.php
+++ app/View/Helper/JournalsHelper.php
@@ -1,27 +1,10 @@
 <?php
-# redMine - project management software
-# Copyright (C) 2006-2008  Jean-Philippe Lang
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-
 class JournalsHelper extends AppHelper
 {
   var $helpers = array(
     'Candy',
     'Html',
-    'Ajax'
+    'Js'
   );
   function render_notes($journal, $current_user, $options=array()) {
     $content = '';
@@ -29,7 +12,7 @@ class JournalsHelper extends AppHelper
     $links = array();
     if($journal['Journal']['notes'] != '') {
       if(!empty($options['reply_links'])) {
-        $links[] = $this->Ajax->link(
+        $links[] = $this->Js->link(
           $this->Html->image('comment.png'),
           array('controller'=>'issues', 'action'=>'reply', 'id'=>$journal['Journal']['journalized_id'], 'journal_id'=>$journal['Journal']['id']),
           array('title'=>__('Quote'))
@@ -37,7 +20,7 @@ class JournalsHelper extends AppHelper
         );
       }
       if($editable) {
-        $links[] = $this->Ajax->link(
+        $links[] = $this->Js->link(
           $this->Html->image('edit.png'), 
           array('controller'=>'journals', 'action'=>'edit', 'id'=>$journal['Journal']['id']),
           array('title'=>__('Edit')/*, 'update'=>"journal-".$journal['Journal']['id']."-notes"*/)
