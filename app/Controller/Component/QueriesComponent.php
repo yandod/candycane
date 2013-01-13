fe93d693005ff17d96b70238dcf8dd0459e1f7c4
Yusuke Ando <yandod@gmail.com>
2013-01-13 12:01:13 +0900

    reformat code

--- app/Controller/Component/QueriesComponent.php
+++ app/Controller/Component/QueriesComponent.php
@@ -1,15 +1,19 @@
 <?php
+/**
+ * QueriesComponent
+ * handle search parameter from query strings or saved filter in db.
+ */
 class QueriesComponent extends Component
 {
-  var $name = 'Queries';
-  var $controller;
-  var $query_filter_cond = array();
-  var $show_filters = array();
+    public $name = 'Queries';
+    public $controller;
+    public $query_filter_cond = array();
+    public $show_filters = array();
   
-  public function startup(Controller $controller)
-  {
-    $this->controller = $controller;
-  }
+    public function startup(Controller $controller)
+    {
+        $this->controller = $controller;
+    }
   
   function retrieve_query($query_id = 0, $forse_set_filter = null)
   {
@@ -144,41 +148,16 @@ class QueriesComponent extends Component
     $this->show_filters = $show_filters;
   }
   
-  function get_option_value($value) {
-    if(is_array($value) && count($value) == 1) {
-      return $this->get_option_value($value[0]);
-    }
-    return $value;
-  }
-#  # Retrieve query from session or build a new query
-#  def retrieve_query
-#    if !params[:query_id].blank?
-#      cond = "project_id IS NULL"
-#      cond << " OR project_id = #{@project.id}" if @project
-#      @query = Query.find(params[:query_id], :conditions => cond)
-#      @query.project = @project
-#      session[:query] = {:id => @query.id, :project_id => @query.project_id}
-#    else
-#      if params[:set_filter] || session[:query].nil? || session[:query][:project_id] != (@project ? @project.id : nil)
-#        # Give it a name, required to be valid
-#        @query = Query.new(:name => "_")
-#        @query.project = @project
-#        if params[:fields] and params[:fields].is_a? Array
-#          params[:fields].each do |field|
-#            @query.add_filter(field, params[:operators][field], params[:values][field])
-#          end
-#        else
-#          @query.available_filters.keys.each do |field|
-#            @query.add_short_filter(field, params[field]) if params[field]
-#          end
-#        end
-#        session[:query] = {:project_id => @query.project_id, :filters => @query.filters}
-#      else
-#        @query = Query.find_by_id(session[:query][:id]) if session[:query][:id]
-#        @query ||= Query.new(:name => "_", :project => @project, :filters => session[:query][:filters])
-#        @query.project = @project
-#      end
-#    end
-#  end
-  
+/**
+ * pick value from whatever single item or multiple items.
+ * @param mixed $value
+ * @return singel value 
+ */
+    public function get_option_value($value)
+    {
+        if(is_array($value) && count($value) == 1) {
+            return $this->get_option_value($value[0]);
+        }
+        return $value;
+  }  
 }
