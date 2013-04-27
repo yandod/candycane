cbe8ed121a09271517d37d5488659f884d91e051
Yuya Takahashi <guriko200906@gmail.com>
2013-04-27 17:30:20 +0900

    added testObjectsCount and testInUse

--- app/Test/Case/Model/EnumerationTest.php
+++ app/Test/Case/Model/EnumerationTest.php
@@ -12,7 +12,7 @@ class EnumerationTestCase extends CakeTestCase
      *
      * @var array
      */
-    public $fixtures = array('app.enumeration');
+    public $fixtures = array('app.enumeration', 'app.tracker', 'app.time_entry', 'app.changeset', 'app.changesets_issue');
 
     /**
      * setUp method
@@ -41,28 +41,27 @@ class EnumerationTestCase extends CakeTestCase
      * testGetValues method
      *
      * @return void
-     * @todo order test
      */
     public function testGetValues()
     {
-        $option = 'IPRI';
         $order = 'ASC';
+        $option = 'IPRI';
         $data = $this->Enumeration->get_values($option, $order);
         $this->assertEqual(1, $data[0]['Enumeration']['position']);
         $this->assertEqual('IPRI', $data[0]['Enumeration']['opt']);
         $extractData = Set::extract("{n}.Enumeration.opt", $data);
         $this->assertEqual(1, count(array_unique($extractData)));
 
-        $option = 'DCAT';
         $order = '';
+        $option = 'DCAT';
         $data = $this->Enumeration->get_values($option, $order);
         $this->assertEqual(1, $data[0]['Enumeration']['position']);
         $this->assertEqual('DCAT', $data[0]['Enumeration']['opt']);
         $extractData = Set::extract("{n}.Enumeration.opt", $data);
         $this->assertEqual(1, count(array_unique($extractData)));
 
-        $option = 'ACTI';
         $order = 'DESC';
+        $option = 'ACTI';
         $data = $this->Enumeration->get_values($option, $order);
         $this->assertEqual(3, $data[0]['Enumeration']['position']);
         $this->assertEqual('ACTI', $data[0]['Enumeration']['opt']);
@@ -93,7 +92,10 @@ class EnumerationTestCase extends CakeTestCase
      */
     public function testObjectsCount()
     {
-
+        $data = $this->Enumeration->find('all');
+        $this->assertEqual(0, $this->Enumeration->objects_count($data[0]));
+        $this->assertEqual(3, $this->Enumeration->objects_count($data[8]));
+        $this->assertEqual(1, $this->Enumeration->objects_count($data[9]));
     }
 
     /**
@@ -103,7 +105,10 @@ class EnumerationTestCase extends CakeTestCase
      */
     public function testInUse()
     {
-
+        $data = $this->Enumeration->find('all');
+        $this->assertFalse($this->Enumeration->in_use($data[0]));
+        $this->assertTrue($this->Enumeration->in_use($data[8]));
+        $this->assertTrue($this->Enumeration->in_use($data[9]));
     }
 
     /**
@@ -113,7 +118,26 @@ class EnumerationTestCase extends CakeTestCase
      */
     public function testDestroy()
     {
+        $this->markTestIncomplete(
+            'このテストは、まだ実装されていません。'
+        );
+    }
 
+    /**
+     * testDeleteOfDestroy method
+     *
+     * @return void
+     */
+    public function testDeleteOfDestroy()
+    {
+        $enumerationId = 4;
+
+        // 削除前
+        $this->assertEqual(1, count($this->Enumeration->findById($enumerationId)));
+        $this->Enumeration->delete($enumerationId);
+
+        // 削除後
+        $this->assertFalse($this->Enumeration->findById($enumerationId));
     }
 
 }
