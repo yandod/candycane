9509e698a4d7b3679dc5c36c6fc374a13108e88c
Yusuke Ando <yandod@gmail.com>
2013-08-06 18:35:56 +0900

    code formatting

--- app/Model/News.php
+++ app/Model/News.php
@@ -2,28 +2,28 @@
 
 class News extends AppModel
 {
-    var $belongsTo = array(
+    public $belongsTo = array(
         'Project' => array(),
-        'Author'  => array(
-            'className'  => 'User',
+        'Author' => array(
+            'className' => 'User',
             'foreginKey' => 'author_id',
         ),
     );
-    var $hasMany = array(
+    public $hasMany = array(
         'Comments' => array(
-            'className'  => 'Comment',
+            'className' => 'Comment',
             'foreignKey' => 'commented_id',
             'conditions' => array('Comments.commented_type' => 'News'),
-            'dependent'  => true
+            'dependent' => true
         )
     );
 
-    var $validate = array(
-        'title'       => array(
-            'Required'  => array('rule' => array('notEmpty'), 'required' => true),
+    public $validate = array(
+        'title' => array(
+            'Required' => array('rule' => array('notEmpty'), 'required' => true),
             'maxLength' => array('rule' => array('maxLength', 60)),
         ),
-        'summary'     => array(
+        'summary' => array(
             'maxLength' => array('rule' => array('maxLength', 255)),
         ),
         'description' => array(
@@ -31,29 +31,35 @@ class News extends AppModel
         ),
     );
 
-    var $actsAs = array(
+    public $actsAs = array(
         'ActivityProvider' => array(
             'find_options' => array('include' => array('Project', 'Author')),
-            'author_key'   => 'author_id'),
-        'Event'            => array('url' => array('Proc' => '_event_url')),
-        'Searchable'       => array()
+            'author_key' => 'author_id'
+        ),
+        'Event' => array('url' => array('Proc' => '_event_url')),
+        'Searchable' => array()
     );
-    var $filterArgs = array(
+    public $filterArgs = array(
         array('name' => 'description', 'type' => 'like'),
         array('name' => 'title', 'type' => 'like'),
     );
 
-    function _event_url($data)
+    public function _event_url($data)
     {
-        return array('controller' => 'news', 'action' => 'show', 'id' => $data['News']['id'], 'project_id' => $data['Project']['id']);
+        return array(
+            'controller' => 'news',
+            'action' => 'show',
+            'id' => $data['News']['id'],
+            'project_id' => $data['Project']['id']
+        );
     }
 
-    function latest($user, $count = 5)
+    public function latest($user, $count = 5)
     {
         $param = array(
-            'order'      => 'News.created_on DESC',
+            'order' => 'News.created_on DESC',
             'conditions' => $this->Project->allowed_to_condition($user, 'view_news'),
-            'limit'      => $count
+            'limit' => $count
         );
         return $this->find('all', $param);
     }
